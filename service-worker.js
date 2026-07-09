58b2bf9c82f2a07d67e07255ef096d4cb78327a0
sobytino <76172854+sobytino@users.noreply.github.com>
2026-07-09 13:46:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-// CroTeak Planner MOBILE BUILD081 PWA service worker
-const CACHE_NAME = 'croteak-mobile-build081-v1';
+// CroTeak Planner MOBILE BUILD082 PWA service worker
+const CACHE_NAME = 'croteak-mobile-build082-v1';
 const APP_SHELL = [
   './',
   './index.html',
-  './CroTeak_Planner_MOBILE_BUILD081.html',
+  './CroTeak_Planner_MOBILE_BUILD082.html',
   './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png',
